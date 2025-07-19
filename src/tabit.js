a78aed9ef528dbb984e5187dc651503827a9459e
dattt-cy <victomblack1602@gmail.com>
2025-07-19 22:57:36 +0700

    Save Hash for Url

--- src/tabit.js
+++ src/tabit.js
@@ -1,4 +1,4 @@
-function Tabit(selector) {
+function Tabit(selector, options) {
     this.container = document.querySelector(selector);
     if (!this.container) {
         console.error("Tabit: Container not found");
@@ -25,12 +25,25 @@ function Tabit(selector) {
     if (this.tabs.length !== this.panels.length) {
         return;
     }
+    this.opts = Object.assign(
+        {
+            remember: false,
+        },
+        options
+    );
     this.originHTML = this.container.innerHTML;
 }
 
 Tabit.prototype._init = function () {
-    const tabActive = this.tabs[0];
-    this._activeTab(tabActive);
+    let tab;
+    const hash = location.hash;
+    tab =
+        (this.opts.remember &&
+            hash &&
+            this.tabs.find((t) => t.getAttribute("href") === hash)) ||
+        this.tabs[0];
+
+    this._activateTab(tab);
 
     this.tabs.forEach((tab) => {
         tab.onclick = (event) => this._handleTabClick(event, tab);
@@ -39,10 +52,10 @@ Tabit.prototype._init = function () {
 
 Tabit.prototype._handleTabClick = function (event, tab) {
     event.preventDefault();
-    this._activeTab(tab);
+    this._activateTab(tab);
 };
 
-Tabit.prototype._activeTab = function (tab) {
+Tabit.prototype._activateTab = function (tab) {
     this.tabs.forEach((tab) => {
         tab.closest("li").classList.remove("tabit--active");
     });
@@ -55,24 +68,30 @@ Tabit.prototype._activeTab = function (tab) {
 
     const panelActive = document.querySelector(tab.getAttribute("href"));
     panelActive.hidden = false;
+
+    if (this.opts.remember) {
+        history.replaceState(null, null, tab.getAttribute("href"));
+    }
 };
 
 Tabit.prototype.switch = function (input) {
-    let tab;
+    let tabToActivate;
     if (typeof input === "string") {
-        tab = this.tabs.find((tab) => tab.getAttribute("href") === input);
-        if (!tab) {
+        tabToActivate = this.tabs.find(
+            (tab) => tab.getAttribute("href") === input
+        );
+        if (!tabToActivate) {
             console.error(`Tabit: Tab with href ${input} not found`);
             return;
         }
     } else if (this.tabs.includes(input)) {
-        tab = input;
-        if (!tab) {
+        tabToActivate = input;
+        if (!tabToActivate) {
             console.error("Tabit: Tab element not found in tabs");
             return;
         }
     }
-    this._activeTab(tab);
+    this._activateTab(tabToActivate);
 };
 
 Tabit.prototype.destroy = function () {
